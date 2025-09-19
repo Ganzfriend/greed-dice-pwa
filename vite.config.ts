809d9cfe714766eb64358f440234aa9718aaeb2c
Ganzfriend <melissaganzfried@gmail.com>
2025-09-19 15:03:11 -0600

    Add Vite PWA magic; Add favicons

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,50 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Greed Game",
+        short_name: "Greed_Game",
+        description: "A six-dice game (aka 10,000/Greed)",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "game-dice-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "game-dice-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "game-dice-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
